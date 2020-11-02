16e9e2d10dcb7f9e009c1fbb5bf8dbb788902ad7
Héctor Cuevas <poncho_nov@hotmail.com>
2020-11-02 03:31:59 -0600

    models for API main response

--- BillBoard/Models/Movie.swift
+++ BillBoard/Models/Movie.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+
+struct Movie: Decodable {
+	var id:Int
+	var popularity : Float
+	var vote_count: Int
+	var video: Bool
+	var poster_path: String
+	var backdrop_path: String
+	var original_language: String
+	var original_title: String
+	var title: String
+	var vote_average: Float
+	var overview: String
+	var release_date: String
+}
--- BillBoard/Models/Response.swift
+++ BillBoard/Models/Response.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+struct Response: Decodable {
+	var page : Int
+	var total_results: Int
+	var total_pages: Int
+	var results: [Movie]
+}
