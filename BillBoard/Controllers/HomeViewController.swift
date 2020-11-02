da2243be253195489e36a9ee7744e9f77eddf02c
Héctor Cuevas <poncho_nov@hotmail.com>
2020-11-02 03:32:37 -0600

    homeview and items configuarion for ipad and iphone

--- BillBoard/Controllers/HomeViewController.swift
+++ BillBoard/Controllers/HomeViewController.swift
@@ -8,22 +8,163 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+	let cellIdentifier = "movieCell"
+	var movies:[Movie] = [Movie]()
+	var isDataLoading = true
+	var refreshControl = UIRefreshControl()
+	
+	lazy var collectionView:UICollectionView = {
+		let l = UICollectionViewFlowLayout()
+		var itemSize = CGSize(width: UIScreen.main.bounds.width/2, height: 300)
+		
+		l.itemSize = itemSize
+		l.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+		l.minimumLineSpacing = 0
+		l.minimumInteritemSpacing = 0
+		
+		let c = UICollectionView(frame: .zero, collectionViewLayout: l)
+		
+		c.translatesAutoresizingMaskIntoConstraints = false
+		c.register(MovieCollectionViewCell.self, forCellWithReuseIdentifier: cellIdentifier)
+		c.dataSource = self
+		c.delegate = self
+		c.backgroundColor = UIColor(red:0.89, green:0.90, blue:0.90, alpha:1.00)
+		
+		return c
+	}()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		self.collectionView.alwaysBounceVertical = true
+		self.refreshControl.tintColor = UIColor.red
+		self.refreshControl.addTarget(self, action: #selector(loadData), for: .valueChanged)
+		self.collectionView.addSubview(refreshControl)
+		setUpView()
+		getInitialData()
+		self.title = "Pop Movies"
+		
+	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+		self.navigationController?.navigationBar.prefersLargeTitles = true
+		setupLayout(width: UIScreen.main.bounds.width)
+	}
+	
+	func setupLayout(width:CGFloat)  {
+		
+		var itemSize:CGSize?
+		var numberOfColumns:CGFloat = 2
+		if UIDevice.current.userInterfaceIdiom == .pad {
+			numberOfColumns = 3
+			if (UIDevice.current.orientation.isLandscape ){
+				numberOfColumns = 5
+			}
+		}
+		itemSize = CGSize(width: width/numberOfColumns, height: 300)
+		
+		let l = UICollectionViewFlowLayout()
+		
+		l.itemSize = itemSize!
+		l.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+		l.minimumLineSpacing = 0
+		l.minimumInteritemSpacing = 0
+		
+		self.collectionView.collectionViewLayout.invalidateLayout()
+		self.collectionView.collectionViewLayout = l
+		self.collectionView.reloadData()
+	}
+	
+	override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+		setupLayout(width: size.width)
+	}
+	
+	func setUpView()  {
+		self.view.backgroundColor = .white
+		
+		self.collectionView.alwaysBounceVertical = true
+		self.refreshControl.tintColor = UIColor.red
+		self.refreshControl.addTarget(self, action: #selector(loadData), for: .valueChanged)
+		self.collectionView.addSubview(refreshControl)
+		
+		
+		[collectionView].forEach(view.addSubview)
+		
+		NSLayoutConstraint.activate([
+			collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			collectionView.trailingAnchor.constraint(equalTo:view.trailingAnchor),
+			collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+			collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+		])
+	}
+	
+	func getInitialData()  {
+		API.shared.getMovies(completion: { (movies, error) in
+			self.movies = movies
+			DispatchQueue.main.async {
+				self.collectionView.reloadData()
+				self.isDataLoading = false
+				self.stopRefresher()
+			}
+		})
+	}
+	
+	@objc func loadData() {
+		refreshControl.beginRefreshing()
+		getInitialData()
+	}
+	
+	func stopRefresher() {
+		refreshControl.endRefreshing()
+	}
+	
+	func scrollViewDidScroll(_ scrollView: UIScrollView) {
+		let contentOffsetY = scrollView.contentOffset.y
+		if contentOffsetY >= (scrollView.contentSize.height - scrollView.bounds.height) - 40 /* Needed offset */ {
+			guard !self.isDataLoading else { return }
+			self.isDataLoading = true
+			
+			API.shared.getMovies(nextPage: true) { (movies, error) in
+				//				self.movies.append(contentsOf: movies)
+				DispatchQueue.main.async {
+					for i in 0 ..< movies.count {
+						DispatchQueue.main.asyncAfter(deadline: .now() + Double(i) * 0.25) {
+							self.movies.append(movies[i])
+							self.collectionView.insertItems(at: [IndexPath(item: self.movies.count - 1, section: 0)])
+						}
+					}
+					
+					self.isDataLoading = false
+				}
+			}
+			
+		}
+	}
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
+extension HomeViewController:UICollectionViewDataSource {
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		
+		return movies.count
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		
+		let cell:MovieCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! MovieCollectionViewCell
+		
+		cell.movie = movies[indexPath.row]
+		
+		return cell
+	}
+	
+	
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+extension HomeViewController:UICollectionViewDelegate {
+	
+	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+		let movie = movies[indexPath.row]
+		//show  movie detail implementation
+	}
 }
--- BillBoard/Views/MovieCollectionViewCell.swift
+++ BillBoard/Views/MovieCollectionViewCell.swift
@@ -8,5 +8,57 @@
 import UIKit
 
 class MovieCollectionViewCell: UICollectionViewCell {
-    
+	
+	lazy var movieImage:UIImageView = {
+		let m = UIImageView(frame: .zero)
+		m.translatesAutoresizingMaskIntoConstraints = false
+
+		return m
+	}()
+	
+	var movie:Movie? {
+		didSet {
+			setUpData()
+		}
+	}
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		setUpView()
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	func setUpData() {
+		guard let movie = self.movie else {return}
+		
+		DispatchQueue.global(qos: .background).async {
+			guard let url = URL(string: "https://image.tmdb.org/t/p/w500\(movie.poster_path )") else {
+				return
+			}
+			if let data = try? Data(contentsOf: url) {
+				
+				DispatchQueue.main.async {
+					
+					let image = UIImage(data: data)
+					self.movieImage.image = image
+				}
+			}
+		}
+	}
+	
+	func setUpView()  {
+		self.backgroundColor = .white
+		[ movieImage].forEach(contentView.addSubview)
+		
+		NSLayoutConstraint.activate([
+			movieImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+			movieImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+			movieImage.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+			movieImage.topAnchor.constraint(equalTo: contentView.topAnchor),
+		])
+	}
+	
 }
